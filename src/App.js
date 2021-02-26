82cc30ab3ce8c139f6a6684abea923bb2e74494d
Eldar <elogon@gmail.com>
2021-02-26 14:12:17 -0500

    Add PageBanner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import PageBanner from './components/PageBanner';
 import SearchResultContainer from './components/SearchResultContainer';
 
 function App() {
   return (
     <div className="App">
+      <PageBanner />
       <SearchResultContainer />
     </div>
   );
---
+++ src/components/PageBanner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PageBanner() {
+  return (
+    <div>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+          <h1 class="display-4">Employee Directory</h1>
+          <h2 class="lead">Click on table headers to filter by heading or use search box to narrow your results.</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageBanner;
